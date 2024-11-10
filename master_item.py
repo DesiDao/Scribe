f303ce2bc30160d8916527fe0c7e85a1c27b9661
DesiDao <55844636+DesiDao@users.noreply.github.com>
2024-11-10 15:10:39 -0500

    Create master_item.py

---
+++ master_item.py
@@ -0,0 +1,50 @@
+from armorNshield import armor_shield
+armors = {
+'Armored kilt':	armor_shield('Light','Armored kilt', 20,	1, 6,	0,	0),
+'Haramaki':	armor_shield('Light','Haramaki',3,1,0,0,0),
+'Quilted cloth': armor_shield('Light','Quilted cloth',100,1,	8,	0,	10),
+'Padded':	armor_shield('Light','Padded',5,1,8,0,5)
+}
+
+armors_Medium ={
+  
+}
+
+armors_Heavy={
+  
+}
+
+weapons_Light = {
+
+}
+
+weapons_One = {
+
+}
+
+weapons_Two={
+
+}
+#wht, name, gp, armor, dex, pen, sp, red, enh = 0
+#str,str,int,int,int,int,int,int
+
+"""
+'Dancing scarves'	150 gp	See text	—	0	—	30 ft.	20 ft.	8 lbs.	PPZO94102
+'Haramaki'	3 gp	+1	—	0	0%	30 ft.	20 ft.	1 lbs.	PRG:UE
+'Padded'	5 gp	+1	+8	0	5%	30 ft.	20 ft.	10 lbs.	PZO1110
+'Quilted cloth'	100 gp	+1	+8	0	10%	30 ft.	20 ft.	15 lbs.	PZO1115
+Reinforced tunic	1 gp	+1	+5	0	5%	30 ft.	20 ft.	5 lbs.	PPZO94102
+Silken ceremonial armor	30 gp	+1	—	0	0%	30 ft.	20 ft.	8 lbs.	PRG:UE
+Lamellar cuirass	15 gp	+2	+4	0	5%	30 ft.	20 ft.	8 lbs.	PRG:UE
+Leather	10 gp	+2	+6	0	10%	30 ft.	20 ft.	15 lbs.	PZO1110
+Rosewood armor	50 gp	+2	+6	0	10%	30 ft.	20 ft.	15 lbs.	PPZO9410
+Hell Knight leather	30 gp	+3	+5	-1	15%	30 ft.	20 ft.	25 lbs.	PRG:AG
+Hide shirt	20 gp	+3	+4	-1	15%	30 ft.	20 ft.	18 lbs.	VBoL
+Leaf armor	500 gp	+3	+5	0	15%	30 ft.	20 ft.	20 lbs.	PCS:ISWG
+Parade armor	25 gp	+3	+5	-1	15%	30 ft.	20 ft.	20 lbs.	PPZO9410
+Spider-silk bodysuit	850 gp	+3	+6	-1	10%	30 ft.	20 ft.	4 lbs.	PPZO94102
+Studded leather	25 gp	+3	+5	-1	15%	30 ft.	20 ft.	20 lbs.	PZO1110
+Wooden	20 gp	+3	+3	-1	15%	30 ft.	20 ft.	25 lbs.	PZO1115
+Chain shirt	100 gp	+4	+4	-2	20%	30 ft.	20 ft.	25 lbs.	PZO1110
+Lamellar (leather)	60 gp	+4	+3	-2	20%	30 ft.	20 ft.	25 lbs.	PRG:UE
+"""
